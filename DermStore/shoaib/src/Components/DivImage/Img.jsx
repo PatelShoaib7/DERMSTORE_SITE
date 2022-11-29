400c4cf5407651d838329249e6694dbbecf2c157
shoaib patel <100205704+PatelShoaib7@users.noreply.github.com>
2022-11-29 16:30:23 +0530

    Update Img.jsx

--- DermStore/shoaib/src/Components/DivImage/Img.jsx
+++ DermStore/shoaib/src/Components/DivImage/Img.jsx
@@ -1,31 +1,29 @@
 import { Box, Grid, GridItem, Image, Text } from '@chakra-ui/react'
 import React from 'react'
 import { textStyles } from '../Body/QuickBuyBottom'
-
-const QuickLast =[{img:"https://static.thcdn.com/images/small/webp/widgets/208-us/43/GHD_676x556-051143.png", title:"Pink Limited Edition Collection" , decp:"Highlitiong awreness year round the collection is delighted  to include a self-check remainder"},
+const QuickLast =[
+                {img:"https://static.thcdn.com/images/small/webp/widgets/208-us/43/GHD_676x556-051143.png", title:"Pink Limited Edition Collection" , decp:"Highlitiong awreness year round the collection is delighted  to include a self-check remainder"},
                 {img:"https://static.thcdn.com/images/small/webp/widgets/208-us/18/0629-STDCRE-38074-DS-CG-July-Priority-Batch-shot_34-HP_LPSQ_BANNER_676x556-032618.jpg", title:"New from Wander Beauty" ,decp:"Highlitiong awreness year round the collection is delighted  to include a self-check remainder"},
                 {img:"https://static.thcdn.com/images/small/webp/widgets/208-us/20/Paula%27s_Choice_676x556_%281%29-052320.png", title:"New from Paula's Choice" , decp:"Highlitiong awreness year round the collection is delighted  to include a self-check remainder"}
 
 ]
 export const Img = () => {
   return (
-    <div>
-      
-          
+    <div>    
       <Grid templateColumns='repeat(3, 1fr)' gap={5} border="1px  black" w="95%" m="auto" h="470px" mt="15px" bg="white">
                 {
                     QuickLast.map((elem)=>
                     (
                       <GridItem>
                                   <Image src={elem.img}/>
-                                  <Box style={textStyles.h1}>{elem.title}</Box>
+                                                    <Box style={textStyles.h1}>
+                                                                              {elem.title}
+                                                     </Box>
                                   <Text>{elem.decp}</Text>
                       </GridItem>
                     ))
                 }
             </Grid>
-
-        
     </div>
   )
 }
