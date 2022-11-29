fedd3d4d58c8f48b9d71c2e0f3465ca5e6d78828
shoaib patel <100205704+PatelShoaib7@users.noreply.github.com>
2022-11-29 16:42:29 +0530

    Update Home.jsx

--- DermStore/shoaib/src/Components/Home/Home.jsx
+++ DermStore/shoaib/src/Components/Home/Home.jsx
@@ -22,65 +22,56 @@ export const Home = () => {
       
     }
     const arr= JSON.parse(localStorage.getItem("formData"))||[];
-
      const save =()=>
-     {
-        
-        console.log(state)
+     {console.log(state)
         arr.push(state)
         localStorage.setItem("formData",JSON.stringify(arr))
-        alert("form is submitted")
-        
+        alert("form is submitted") 
      }
     return (
-      <>
-     
-        <Modal finalFocusRef={finalRef} isOpen={isOpen} onClose={onClose}>
+      <> <Modal finalFocusRef={finalRef} isOpen={isOpen} onClose={onClose}>
           <ModalOverlay />
           <ModalContent>
             <ModalHeader>Modal Title</ModalHeader>
             <ModalCloseButton />
             <ModalBody >
-                
-               
-              {/* <Lorem count={2} /> */}
-            
-<InputGroup>      
+ 
+                    <InputGroup>      
 
-                  <Input    mb='2rem' type='text'  
-                            name="name"  placeholder='Enter Name' 
-                            onChange={Info}
-                            value={state.name} />
- </InputGroup>
+                                      <Input    mb='2rem' type='text'  
+                                                name="name"  placeholder='Enter Name' 
+                                                onChange={Info}
+                                                value={state.name} />
+                     </InputGroup>
 
 
- <InputGroup>
-        <InputLeftElement   pointerEvents='none' children={<EmailIcon color='gray.300' />}/>
-                  <Input    mb='2rem' type='tel' name="email" 
-                            placeholder='Enter email'  
-                            onChange={Info} 
-                            value={state.email}/>
-  </InputGroup>
+                         <InputGroup>
+                                <InputLeftElement   pointerEvents='none' children={<EmailIcon color='gray.300' />}/>
+                                          <Input    mb='2rem' type='tel' name="email" 
+                                                    placeholder='Enter email'  
+                                                    onChange={Info} 
+                                                    value={state.email}/>
+                          </InputGroup>
 
 
-  <InputGroup>
-          <InputLeftElement pointerEvents='none' children={<PhoneIcon color='gray.300' />}/>
-               <Input       mb='2rem' type='tel' name="num" 
-                            placeholder='Enter num'  
-                            onChange={Info} 
-                            value={state.num}/>
-         {/* <InputLeftElement  pointerEvents='none' children={<PhoneIcon color='gray.300' />}/> */}
-  </InputGroup>
+                          <InputGroup>
+                                  <InputLeftElement pointerEvents='none' children={<PhoneIcon color='gray.300' />}/>
+                                       <Input       mb='2rem' type='tel' name="num" 
+                                                    placeholder='Enter num'  
+                                                    onChange={Info} 
+                                                    value={state.num}/>
+                                 {/* <InputLeftElement  pointerEvents='none' children={<PhoneIcon color='gray.300' />}/> */}
+                          </InputGroup>
               
-            </ModalBody>
-  
-            <ModalFooter>
-              <Button colorScheme='blue' mr={3} onClick={onClose}>
-                Close
-              </Button>
-              <Button onClick={save}   variant='ghost'>Submit</Button>
-            </ModalFooter>
-          </ModalContent>
+                            </ModalBody>
+
+                                        <ModalFooter>
+                                          <Button colorScheme='blue' mr={3} onClick={onClose}>
+                                            Close
+                                          </Button>
+                                          <Button onClick={save}   variant='ghost'>Submit</Button>
+                                        </ModalFooter>
+                          </ModalContent>
         </Modal>
         <Navbar />
        <NavAllPages />
