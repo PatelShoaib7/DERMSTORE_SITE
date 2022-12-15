107041055577775a90515085e40aa41228c07663
shoaib patel <100205704+PatelShoaib7@users.noreply.github.com>
2022-12-15 08:27:56 +0530

    Update NavAllPages.jsx

--- DermStore/shoaib/src/Components/Navbar/NavAllPages.jsx
+++ DermStore/shoaib/src/Components/Navbar/NavAllPages.jsx
@@ -13,78 +13,60 @@ export const NavAllPages = () => {
        
   <Flex border="1px  grey" w="95%" m="auto" gap="10px" h="70px" p="9px">
       <Menu p="6px">
-          <MenuButton  className={styles.Navbar_Flex3} fontSize="16px"  rightIcon={<ChevronDownIcon />} borderBottom="1px  blue">
-            Brands  <ChevronDownIcon w={6} h={5}  />
-          </MenuButton>
-          <MenuList>
-            <MenuItem>Download</MenuItem>
-            <MenuItem>Create a Copy</MenuItem>
-            <MenuItem>Mark as Draft</MenuItem>
-            <MenuItem>Delete</MenuItem>
-            <MenuItem>Attend a Workshop</MenuItem>
-          </MenuList>
+              <MenuButton  className={styles.Navbar_Flex3} fontSize="16px"  rightIcon={<ChevronDownIcon />} borderBottom="1px  blue">
+                Brands  <ChevronDownIcon w={6} h={5}  />
+              </MenuButton>
+              <MenuList>
+                <MenuItem>Download</MenuItem>
+                <MenuItem>Create a Copy</MenuItem>
+                <MenuItem>Mark as Draft</MenuItem>
+                <MenuItem>Delete</MenuItem>
+                <MenuItem>Attend a Workshop</MenuItem>
+              </MenuList>
       </Menu>
-
-
-    {/* <Spacer /> */}
     <Menu>
-        <MenuButton   className={styles.Navbar_Flex3} fontSize="16px"  rightIcon={<ChevronDownIcon />} borderBottom="1px  blue">
-          Summer Shop  <ChevronDownIcon w={6} h={5}  />
-        </MenuButton>
-        <MenuList>
-          <MenuItem>Download</MenuItem>
-          <MenuItem>Create a Copy</MenuItem>
-          <MenuItem>Mark as Draft</MenuItem>
-          <MenuItem>Delete</MenuItem>
-          <MenuItem>Attend a Workshop</MenuItem>
-        </MenuList>
+            <MenuButton   className={styles.Navbar_Flex3} fontSize="16px"  rightIcon={<ChevronDownIcon />} borderBottom="1px  blue">
+              Summer Shop  <ChevronDownIcon w={6} h={5}  />
+            </MenuButton>
+              <MenuList>
+                <MenuItem>Download</MenuItem>
+                <MenuItem>Create a Copy</MenuItem>
+                <MenuItem>Mark as Draft</MenuItem>
+                <MenuItem>Delete</MenuItem>
+                <MenuItem>Attend a Workshop</MenuItem>
+              </MenuList>
     </Menu>
-
-
-
-    {/* <Spacer /> */}
     <Menu>
-        <MenuButton  className={styles.Navbar_Flex3} fontSize="16px"  rightIcon={<ChevronDownIcon />} borderBottom="1px  blue">
-          Browser By  <ChevronDownIcon w={6} h={5}  />
-        </MenuButton>
-        <MenuList>
-          <MenuItem>Download</MenuItem>
-          <MenuItem>Create a Copy</MenuItem>
-        </MenuList>
+                <MenuButton  className={styles.Navbar_Flex3} fontSize="16px"  rightIcon={<ChevronDownIcon />} borderBottom="1px  blue">
+                  Browser By  <ChevronDownIcon w={6} h={5}  />
+                </MenuButton>
+                <MenuList>
+                  <MenuItem>Download</MenuItem>
+                  <MenuItem>Create a Copy</MenuItem>
+                </MenuList>
     </Menu>
-  
-  
-
-    {/* <Spacer /> */}
-    <Menu bordr="1px  black">
-        <MenuButton  className={styles.Navbar_Flex3}  bordr="1px  black" fontSize="16px"  rightIcon={<ChevronDownIcon />} borderBottom="1px  blue">
-          BsetSeller  <ChevronDownIcon w={6} h={5}  />
-        </MenuButton>
-          
-        <MenuList  bordr="1px  black">
-          
-            <MenuItem>Download</MenuItem>
-          <MenuItem>Create a Copy</MenuItem>
-            <MenuItem>Attend a Workshop</MenuItem>
-         
+                <Menu bordr="1px  black">
+                        <MenuButton  className={styles.Navbar_Flex3}  bordr="1px  black" fontSize="16px"  rightIcon={<ChevronDownIcon />} borderBottom="1px  blue">
+                          BsetSeller  <ChevronDownIcon w={6} h={5}  />
+                        </MenuButton>
+                 <MenuList  bordr="1px  black">
+                 <MenuItem>Download</MenuItem>
+                 <MenuItem>Create a Copy</MenuItem>
+               <MenuItem>Attend a Workshop</MenuItem>
         </MenuList>
        
         
     </Menu>
-  
-  
-  
-
     {/* <Spacer /> */}
     <Menu>
-        <MenuButton  className={styles.Navbar_Flex3} fontSize="16px"  rightIcon={<ChevronDownIcon />} borderBottom="1px  blue">
-          Skin Care  <ChevronDownIcon w={6} h={5}  />
-        </MenuButton>
-        <MenuList>
-          <MenuItem>Download</MenuItem>
-          <MenuItem>Create a Copy</MenuItem>
-          <MenuItem>Mark as Draft</MenuItem>
-        </MenuList>
+            <MenuButton  className={styles.Navbar_Flex3} fontSize="16px"  rightIcon={<ChevronDownIcon />} borderBottom="1px  blue">
+                    Skin Care  <ChevronDownIcon w={6} h={5}  />
+            </MenuButton>
+              <MenuList>
+                <MenuItem>Download</MenuItem>
+                <MenuItem>Create a Copy</MenuItem>
+                <MenuItem>Mark as Draft</MenuItem>
+              </MenuList>
     </Menu>
    
    
@@ -92,103 +74,94 @@ export const NavAllPages = () => {
 
     {/* <Spacer /> */}
     <Menu>
-        <MenuButton  className={styles.Navbar_Flex3} fontSize="16px"  rightIcon={<ChevronDownIcon />} borderBottom="1px  blue">
-          Make Up  <ChevronDownIcon w={6} h={5}  />
-        </MenuButton>
-        <MenuList>
-          <MenuItem>Download</MenuItem>
-          <MenuItem>Create a Copy</MenuItem>
-          <MenuItem>Mark as Draft</MenuItem>
-        
-        </MenuList>
+              <MenuButton  className={styles.Navbar_Flex3} fontSize="16px"  rightIcon={<ChevronDownIcon />} borderBottom="1px  blue">
+                Make Up  <ChevronDownIcon w={6} h={5}  />
+              </MenuButton>
+              <MenuList>
+                <MenuItem>Download</MenuItem>
+                <MenuItem>Create a Copy</MenuItem>
+                <MenuItem>Mark as Draft</MenuItem>
+            </MenuList>
     </Menu>
    
    
     <Menu>
-        <MenuButton  className={styles.Navbar_Flex3} fontSize="16px"  rightIcon={<ChevronDownIcon />} borderBottom="1px  blue">
-          Hiar Care  <ChevronDownIcon w={6} h={5}  />
-        </MenuButton>
-        <MenuList>
-          <MenuItem>Download</MenuItem>
-          <MenuItem>Create a Copy</MenuItem>
-          <MenuItem>Mark as Draft</MenuItem>
-          
-        </MenuList>
+            <MenuButton  className={styles.Navbar_Flex3} fontSize="16px"  rightIcon={<ChevronDownIcon />} borderBottom="1px  blue">
+              Hiar Care  <ChevronDownIcon w={6} h={5}  />
+            </MenuButton>
+            <MenuList>
+              <MenuItem>Download</MenuItem>
+              <MenuItem>Create a Copy</MenuItem>
+              <MenuItem>Mark as Draft</MenuItem>
+            </MenuList>
     </Menu>
 
 
     <Menu>
-        <MenuButton  className={styles.Navbar_Flex3} fontSize="16px"  rightIcon={<ChevronDownIcon />} borderBottom="1px  blue">
-          Bath Body  <ChevronDownIcon w={6} h={5}  />
-        </MenuButton>
-        <MenuList>
-          <MenuItem>Download</MenuItem>
-          <MenuItem>Create a Copy</MenuItem>
-          <MenuItem>Mark as Draft</MenuItem>
-          
-        </MenuList>
+              <MenuButton  className={styles.Navbar_Flex3} fontSize="16px"  rightIcon={<ChevronDownIcon />} borderBottom="1px  blue">
+                Bath Body  <ChevronDownIcon w={6} h={5}  />
+              </MenuButton>
+              <MenuList>
+                <MenuItem>Download</MenuItem>
+                <MenuItem>Create a Copy</MenuItem>
+                <MenuItem>Mark as Draft</MenuItem>
+
+              </MenuList>
     </Menu>
     <Menu>
-        <MenuButton  className={styles.Navbar_Flex3} fontSize="16px"  rightIcon={<ChevronDownIcon />} borderBottom="1px  blue">
-          Tool {`&`} Device  <ChevronDownIcon w={6} h={5}  />
-        </MenuButton>
-        <MenuList>
-          <MenuItem>Download</MenuItem>
-          <MenuItem>Create a Copy</MenuItem>
-          <MenuItem>Mark as Draft</MenuItem>
-          
-        </MenuList>
+              <MenuButton  className={styles.Navbar_Flex3} fontSize="16px"  rightIcon={<ChevronDownIcon />} borderBottom="1px  blue">
+                Tool {`&`} Device  <ChevronDownIcon w={6} h={5}  />
+              </MenuButton>
+            <MenuList>
+              <MenuItem>Download</MenuItem>
+              <MenuItem>Create a Copy</MenuItem>
+              <MenuItem>Mark as Draft</MenuItem>
+            </MenuList>
     </Menu>
     <Menu>
-        <MenuButton  className={styles.Navbar_Flex3} fontSize="16px"  rightIcon={<ChevronDownIcon />} borderBottom="1px  blue">
-          Gifts {`&`}  Sets  <ChevronDownIcon w={6} h={5}  />
-        </MenuButton>
-        <MenuList>
-          <MenuItem>Download</MenuItem>
-          <MenuItem>Create a Copy</MenuItem>
-          <MenuItem>Mark as Draft</MenuItem>
-          
-        </MenuList>
+              <MenuButton  className={styles.Navbar_Flex3} fontSize="16px"  rightIcon={<ChevronDownIcon />} borderBottom="1px  blue">
+                Gifts {`&`}  Sets  <ChevronDownIcon w={6} h={5}  />
+              </MenuButton>
+              <MenuList>
+                <MenuItem>Download</MenuItem>
+                <MenuItem>Create a Copy</MenuItem>
+                <MenuItem>Mark as Draft</MenuItem>
+              </MenuList>
     </Menu>
     <Menu>
-        <MenuButton  className={styles.Navbar_Flex3} fontSize="16px"  rightIcon={<ChevronDownIcon />} borderBottom="1px  blue">
-          Beauty {`&`}  Fix  <ChevronDownIcon w={6} h={5}  />
-        </MenuButton>
-        <MenuList>
-          <MenuItem>Download</MenuItem>
-          <MenuItem>Create a Copy</MenuItem>
-          <MenuItem>Mark as Draft</MenuItem>
-          
-        </MenuList>
+            <MenuButton  className={styles.Navbar_Flex3} fontSize="16px"  rightIcon={<ChevronDownIcon />} borderBottom="1px  blue">
+              Beauty {`&`}  Fix  <ChevronDownIcon w={6} h={5}  />
+            </MenuButton>
+            <MenuList>
+              <MenuItem>Download</MenuItem>
+              <MenuItem>Create a Copy</MenuItem>
+              <MenuItem>Mark as Draft</MenuItem>
+            </MenuList>
     </Menu>
 
     <Menu>
-        <MenuButton  className={styles.Navbar_Flex3} fontSize="16px"  rightIcon={<ChevronDownIcon />} borderBottom="1px  blue">
-          Offers  <ChevronDownIcon w={6} h={5}  />
-        </MenuButton>
-        <MenuList>
-          <MenuItem>Download</MenuItem>
-          <MenuItem>Create a Copy</MenuItem>
-          <MenuItem>Mark as Draft</MenuItem>
-          
-        </MenuList>
+                <MenuButton  className={styles.Navbar_Flex3} fontSize="16px"  rightIcon={<ChevronDownIcon />} borderBottom="1px  blue">
+                  Offers  <ChevronDownIcon w={6} h={5}  />
+                </MenuButton>
+                <MenuList>
+                    <MenuItem>Download</MenuItem>
+                    <MenuItem>Create a Copy</MenuItem>
+                    <MenuItem>Mark as Draft</MenuItem>
+               </MenuList>
     </Menu>
 
     <Menu>
-        <MenuButton  className={styles.Navbar_Flex3} fontSize="16px"  rightIcon={<ChevronDownIcon />} borderBottom="1px  blue">
-          New  <ChevronDownIcon w={6} h={5}  />
-        </MenuButton>
-        <MenuList>
-          <MenuItem>Download</MenuItem>
-          <MenuItem>Create a Copy</MenuItem>
-          <MenuItem>Mark as Draft</MenuItem>
-          
-        </MenuList>
-    </Menu>
+              <MenuButton  className={styles.Navbar_Flex3} fontSize="16px"  rightIcon={<ChevronDownIcon />} borderBottom="1px  blue">
+                New  <ChevronDownIcon w={6} h={5}  />
+              </MenuButton>
+              <MenuList>
+                <MenuItem>Download</MenuItem>
+                <MenuItem>Create a Copy</MenuItem>
+                <MenuItem>Mark as Draft</MenuItem>
 
-   
+              </MenuList>
+    </Menu>
   </Flex>
-
   
      <BigImage />
  
