1842a85bde90066a851fda76142ab1178312d33a
shoaib patel <100205704+PatelShoaib7@users.noreply.github.com>
2022-12-16 12:03:09 +0530

    Update AppContext.jsx

--- DermStore/shoaib/src/Context/AppContext.jsx
+++ DermStore/shoaib/src/Context/AppContext.jsx
@@ -3,17 +3,22 @@ import { createContext } from "react";
 import { Navigate, useNavigate } from "react-router-dom";
 export const  AppContex=createContext()
 export const AppContexProvider =({children})=>
-{ const [count , setCount]=useState(0)
+{ const [count , setCount]=useState(0);
+ const [status , setStatus]= useState(false)
   const handleClick=()=>
-  {
-    setCount(()=> count +1)
-    alert("Item Added To Cart")
-     Navigate("/home/supershop/cart")
+  {   setCount(()=> count +1)
+      alert("Item Added To Cart")
+      Navigate("/home/supershop/cart")
   }
   const handleDec=()=>
   {
     setCount(()=> count -1) 
   }
+   const handdlStatus =()=>{
+      let upDtaed_Status = ! status ;
+      setStatu(upDtaed_Status  )
+     
+   }
    return
   (<AppContex.Provider value={{count ,handleClick,handleDec }}>
     {children}
